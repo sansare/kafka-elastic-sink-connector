9aeb32f155e8ed11d3c7e1896e57156a9eb40877
Artem Astakhov <aiastahov@gmail.com>
2018-04-28 12:04:47 +0300

    add currency id

--- src/main/java/com/skynyrd/kafka/transform/impl/BaseProductsRecordTransformer.java
+++ src/main/java/com/skynyrd/kafka/transform/impl/BaseProductsRecordTransformer.java
@@ -41,6 +41,7 @@ public class BaseProductsRecordTransformer extends AbstractRecordTransformer {
         JsonObject docJson = new JsonObject();
         docJson.addProperty("id", payload.get("id").getAsLong());
         docJson.addProperty("category_id", payload.get("category_id").getAsLong());
+        docJson.addProperty("currency_id", payload.get("currency_id").getAsLong());
         docJson.add(
                 "name",
                 gson.fromJson(payload.get("name").getAsString(), JsonArray.class)
